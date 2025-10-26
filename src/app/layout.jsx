c9fb6cbeda2fe37784c09f16460bd29872665031
beeztechstudios <beeztech.studios@gmail.com>
2025-10-26 23:41:29 +0530

    added google_verification_link for google search console

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -332,6 +332,7 @@ export default function RootLayout({ children }) {
       href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Libre+Franklin:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Volkhov:ital,wght@0,400;0,700;1,400;1,700&display=swap"
       rel="stylesheet"
     />
+        <meta name="google-site-verification" content="h8AVtcYpu9JM7KWRC5j7tL6dVHEFS7p8wD_Y1fArAgY" />
       </head>
 
       <body >
